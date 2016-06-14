269767c9512990eb4ca69d70f78866aeb5835460
Erica Winberry <elaewin@gmail.com>
2016-06-14 14:53:23 -0700

    Updates from class: first question in the js, initial content in html, very basic css.

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
-alert('YAY!');
+var userName = prompt('Hello! What is your name?');
+alert('Welcome ' + userName + '! Glad you\'re here!');
+
+var answer1 = prompt('Does Erica have 2 cats? (yes or no)').toLowerCase();
+
+if (answer1 === 'yes' || answer1 === 'y') {
+  alert('You\'re right! Erica has two cats, named Aonghus and Murdoch. She calls them her \'Scottish Buddy-Cop Show\'.');
+} else {
+  alert('Wrong! Erica does have two cats.');
+}
